0622ee10705bac0bcf8369087bca5badaf3e0679
Tobby Echonga <30405173+TobbyEchonga@users.noreply.github.com>
2024-01-18 01:49:02 +0300

    Add files via upload

---
+++ recipe_app/app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from app import db
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
